91fc1d952cc12df48ab44ba5864185c360c73171
BishalDey908 <ronydey34567@gmail.com>
2024-06-27 15:15:50 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const friendRequestAcceptModel = require("./firendApprovedModel")
 PORT= 3000
 app.use(bodyParser.json())
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://social-media-mern-frontend.vercel.app"],
   methods: ["GET", "POST"],
   credentials: true,
 }))
